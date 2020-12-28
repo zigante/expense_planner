1e0a1923a78b4728d9e0986fca82b2a4d919bc3b
zigante <zigante.pedro@gmail.com>
2020-12-28 20:02:22 -0300

    add static list and static text input

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground'),
-      ),
+    initializeDateFormatting('pt_BR');
+    return MaterialApp(
+      home: MyHomePage(),
+      title: 'Flutter App',
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 'tx1', title: 'Tênis novo', amount: 299.99, date: DateTime.now()),
+    Transaction(id: 'tx2', title: 'Camiseta nova', amount: 79.99, date: DateTime.now()),
+    Transaction(id: 'tx3', title: 'Jeans', amount: 119.99, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Text Control'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              child: Container(
+                color: Colors.blue,
+                child: Text('Chart'),
+                width: double.infinity,
+              ),
+              elevation: 5,
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(decoration: InputDecoration(labelText: 'Título')),
+                    TextField(decoration: InputDecoration(labelText: 'Valor')),
+                    FlatButton(onPressed: () => null, child: Text('Adicionar'), textColor: Colors.purple)
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: this
+                  .transactions
+                  .map(
+                    (e) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                            decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
+                            child: Text(
+                              'R\$ ${e.amount}',
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                e.title,
+                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                new DateFormat.yMMMMd('pt_BR').format(e.date),
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            )
+          ],
+        ));
   }
 }
