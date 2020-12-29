46bc1bbdf1ce3a36c1adc88c1f326f0fa0a891b6
zigante <zigante.pedro@gmail.com>
2020-12-29 11:38:18 -0300

    using list tile instead of container and card to display list items

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -29,34 +29,59 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (_context, _index) {
                 final currentTransaction = this._transactions[_index];
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
-                        child: Text(
-                          'R\$ ${currentTransaction.amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20, color: Theme.of(context).primaryColor),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('R\$ ${currentTransaction.amount.toStringAsFixed(2)}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            currentTransaction.title,
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            new DateFormat.yMMMMd('pt_BR').format(currentTransaction.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(currentTransaction.title, style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(new DateFormat.yMMMMd('pt_BR').format(currentTransaction.date)),
+                    // trailing: IconButton(
+                    //   icon: Icon(Icons.delete),
+                    //   onPressed: () => null,
+                    //   color: Colors.red,
+                    // ),
                   ),
                 );
+                // return Card(
+                //   child: Row(
+                //     children: [
+                //       Container(
+                //         padding: EdgeInsets.all(10),
+                //         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                //         decoration: BoxDecoration(border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
+                //         child: Text(
+                //           'R\$ ${currentTransaction.amount.toStringAsFixed(2)}',
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: [
+                //           Text(
+                //             currentTransaction.title,
+                //             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                //           ),
+                //           Text(
+                //             new DateFormat.yMMMMd('pt_BR').format(currentTransaction.date),
+                //             style: TextStyle(color: Colors.grey),
+                //           ),
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               },
               itemCount: this._transactions.length,
             ),
